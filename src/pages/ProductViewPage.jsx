7e2f2f905a820a6162b29699e5cc1a8b22e50df6
PH <paulofera159@gmail.com>
2024-07-26 01:06:06 -0300

    Alterações Finais | Começando composição das páginas

--- src/pages/ProductViewPage.jsx
+++ src/pages/ProductViewPage.jsx
@@ -1,10 +1,24 @@
+import BuyBox from "../Components/BuyBox/BuyBox";
 import GalleryProduct from "../Components/Gallery/GalleryProduct";
+import ProductCard from "../Components/ProductCard/ProductCard";
+import ProductListing from "../Components/ProductListing/ProductListing";
+import Section from "../Components/Section/Section";
 import Layout from "./Layout";
+
+import './ProductViewPage.css'
 
 export default function ProductViewPage() {
     return (
-        <Layout>
-            <GalleryProduct/>
+        <Layout posicao={1}>
+            <div className="ViewPageLinks">
+                <p className="LinksDestaque">Home</p>
+                <p className="LinksRestante"> &nbsp;/ Produtos / Tênis / Nike / Tênis Nike Revolution 6 Next Nature Masculino</p>
+            </div>
+            <div className="ViewPageSeparacao">
+                <GalleryProduct />
+                <BuyBox/>
+            </div>
+            <Section id="HomePage"><ProductListing title="Produtos em alta" children={<ProductCard quantidade={2} />} /></Section>
         </Layout>
     )
 }
